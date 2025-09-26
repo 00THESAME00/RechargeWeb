7dc96aebb3993762f1e45751c0509aba31bc6418
Emil Onuchin <onuchinemil@gmail.com>
2025-09-27 00:36:52 +0500

    update: removed image and rebuilt site

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "51683ce7074de88e6e7f46942120d6b9
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "3982613f17c1a5c467dd9e44d45a98a0",
+"flutter_bootstrap.js": "e5f59aa081369d807bcd091035ba5de6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
