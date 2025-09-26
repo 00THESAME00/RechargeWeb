31c58da6166521fcb0948419d9a2f2c33156bc8f
Emil Onuchin <onuchinemil@gmail.com>
2025-09-26 20:35:20 +0500

    Fix url_launcher for Dart 3.0.6

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "51683ce7074de88e6e7f46942120d6b9
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "108122c9fb8787ec74f4136d7d4c7c4f",
+"flutter_bootstrap.js": "661e775921f42d313a74deb1ca43674f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
