6f38846d060a72c401d079386633f43518a0882e
Emil Onuchin <onuchinemil@gmail.com>
2025-09-26 18:47:57 +0500

    Fix flutter_lints for Dart 3.0.6

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "51683ce7074de88e6e7f46942120d6b9
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5ca35ee9714e7fcd2bfea8eed2beed57",
+"flutter_bootstrap.js": "55c225135f946c607f889efa6f14bbdd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
